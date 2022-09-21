0d3fe15e2308f5983b6ceebafdc7567a56b17c5d
Jesús González <jesus.gonzalez@corhuila.edu.co>
2022-09-21 06:23:42 -0500

    Ejemplos html s2

---
+++ Semana2/Sesion4/Ideal/js/funciones.js
@@ -0,0 +1,8 @@
+alert("Buenos días");
+
+function Calcular(){
+      var n1 = parseFloat(prompt("Digite un número "));
+      var n2 = parseFloat(prompt("Digite un número "));  
+      var suma = n1+n2;
+      alert("la suma es "+suma)
+}
